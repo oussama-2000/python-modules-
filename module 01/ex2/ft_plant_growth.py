4dedc137bccf5a1bda3917dcadfd39fb84adf323
Oussama Amkhou <163328646+oussama-2000@users.noreply.github.com>
2026-01-17 08:36:06 +0100

    Refactor Plant class methods and add type hints

--- module 01/ex2/ft_plant_growth.py
+++ module 01/ex2/ft_plant_growth.py
@@ -1,24 +1,48 @@
 class Plant:
-    def __init__(self, name, height, age):
+    """plant class"""
+
+    def __init__(self, name: str, height: int, age: int):
         self.name = name
         self.height = height
         self.agee = age
 
-    def get_info(self):
+    def get_info(self) -> None:
+        """shows plant informations"""
+
         print(f"{self.name}: {self.height}cm, {self.agee} days old")
 
-    def grow(self):
-        self.height += 6
+    def grow(self, value: int) -> None:
+        """grows the plant"""
 
-    def age(self):
-        self.agee += 6
+        self.height += value
+
+    def age(self, value: int) -> None:
+        """increase the plant age"""
+
+        self.agee += value
 
 
-plant = Plant("Rose", 25, 30)
-print("=== Day 1 ===")
-plant.get_info()
-plant.grow()
-plant.age()
-print("=== Day 7 ===")
-plant.get_info()
-print("Growth this week: +6cm")
+def programe_test() -> None:
+
+    plant = Plant("Rose", 25, 30)
+
+    prev_height = plant.height
+
+    growth_value = 6
+    aging_value = 6
+
+    print("=== Day 1 ===")
+
+    plant.get_info()
+    plant.grow(growth_value)
+    plant.age(aging_value)
+
+    print(f"=== Day {aging_value + 1} ===")
+    plant.get_info()
+
+    growth = plant.height - prev_height
+    print(f"Growth this week: +{growth}cm")
+
+
+if __name__ == "__main__":
+    programe_test()
