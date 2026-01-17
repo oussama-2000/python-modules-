c260b89d3fdf54ad32a3fb1b38abc1b6e12b1a90
Oussama Amkhou <163328646+oussama-2000@users.noreply.github.com>
2026-01-17 08:38:33 +0100

    Refactor plant classes to secure properties

--- module 01/ex4/ft_garden_security.py
+++ module 01/ex4/ft_garden_security.py
@@ -1,101 +1,66 @@
-class Plant:
-    """plant class with common features"""
-    def __init__(self, name: str, height: int, age: int):
+class SecurePlant:
+    """secure plant class"""
+
+    def __init__(self, name: str, height: str, age: int):
         self.name = name
-        self.height = height
-        self.age = age
+        self.__height = None
+        self.__age = None
+        self.set_height(height, 0)
+        self.set_age(age, 0)
+
+    def set_height(self, new_height: int, message: int) -> None:
+        """setter for set height prorprity"""
+
+        if (new_height > 0):
+            self.__height = new_height
+            if message:
+                print(f"Height updated: {self.__height}cm [OK]")
+        else:
+            print("Invalid operation attempted: ", end="")
+            print(f"height {new_height}cm [REJECTED]")
+            print("Security: Negative height rejected")
+
+    def set_age(self, new_age: int, message: int):
+        """setter for set age protprity"""
+
+        if (new_age > 0):
+            self.__age = new_age
+            if message:
+                print(f"Age updated: {self.__age} days [OK]")
+        else:
+            print("Invalid operation attempted: ", end="")
+            print(f"age {new_age}cm [REJECTED]")
+            print("Security: Negative age rejected")
+
+    def get_height(self):
+        """getter for get height proprity"""
+
+        return self.__height
+
+    def get_age(self):
+        """getter for get age proprity"""
+
+        return self.__age
 
 
-class Flower(Plant):
-    """flower plant type class"""
+def program_test() -> None:
 
-    def __init__(self, name: str, height: int, age: int, color: str):
-        super().__init__(name, height, age)
-        self.color = color
+    print("=== Garden Security System ===")
+    plant = SecurePlant("Rose", 19, 28)
+    print(f"Plant created: {plant.name}")
 
-    def bloom(self) -> None:
-        """bloom ability method"""
+    plant.set_height(25, 1)
+    plant.set_age(30, 1)
 
-        print(f"{self.name} is blooming beautifully!")
-
-    def show_char(self) -> None:
-        print(f"{self.name} (Flower): {self.height}cm,"
-              f" {self.age} days, {self.color} color")
-
-
-class Tree(Plant):
-    """tree plant type class"""
-
-    def __init__(
-            self,
-            name: str,
-            height: str,
-            age: str,
-            trunk_diameter: int
-            ):
-
-        super().__init__(name, height, age)
-        self.trunk_diameter = trunk_diameter
-
-    def produce_shade(self) -> None:
-        """produce shade ability method"""
-
-        shade_area = self.trunk_diameter * 1.56
-        print(f"{self.name} provides {shade_area:.0f} square meters of shade")
-
-    def show_char(self) -> None:
-        print(f"{self.name} (Tree): {self.height}cm,"
-              f" {self.age} days, {self.trunk_diameter}cm diameter")
-
-
-class Vegetable(Plant):
-    """vegetable plant type class"""
-
-    def __init__(
-            self,
-            name: str,
-            height: int,
-            age: int,
-            harvest_season: str,
-            nutritional_value: str
-            ):
-
-        super().__init__(name, height, age)
-        self.season = harvest_season
-        self.nutritional_value = nutritional_value
-
-    def show_nutritional_value(self) -> None:
-        """shows nutritional value method"""
-
-        print(f"{self.name} is rich in {self.nutritional_value}")
-
-    def show_char(self) -> None:
-        print(f"{self.name} (Vegetable): {self.height}cm,"
-              f" {self.age} days, {self.season} harvest")
-
-
-def protram_test() -> None:
-
-    print("=== Garden Plant Types ===", end="\n\n")
-
-    rose = Flower("Rose", 25, 30, "red")
-    _ = Flower("Tulip", 20, 25, "yellow")
-
-    oak = Tree("Oak", 500, 1825, 50)
-    _ = Tree("Orange", 100, 300, 40)
-
-    tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
-    _ = Vegetable("Carrot", 30, 70, "spring", "vitamin A")
-
-    rose.show_char()
-    rose.bloom()
     print("\n")
-    oak.show_char()
-    oak.produce_shade()
+
+    plant.set_height(-5, 1)
+
     print("\n")
-    tomato.show_char()
-    tomato.show_nutritional_value()
+
+    print(f"Current plant: {plant.name} ({plant.get_height()}cm, "
+          f"{plant.get_age()} days)")
 
 
 if __name__ == "__main__":
-    protram_test()
+    program_test()
