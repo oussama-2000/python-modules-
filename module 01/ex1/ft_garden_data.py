f05471671d940e382ff2bcec8d891bc58bbb2daf
Oussama Amkhou <163328646+oussama-2000@users.noreply.github.com>
2026-01-17 08:35:41 +0100

    Update ft_garden_data.py

--- module 01/ex1/ft_garden_data.py
+++ module 01/ex1/ft_garden_data.py
@@ -1,17 +1,24 @@
 class Plant:
-    def __init__(self, name, height, age):
+    """plant class"""
+    def __init__(self, name: str, height: int, age: int):
         self.name = name
         self.height = height
         self.age = age
 
-    def show(self):
+    def show(self) -> None:
+        """shows plant informations"""
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
-plant_1 = Plant("Rose", 25, 30)
-plant_2 = Plant("Sunflower", 80, 45)
-plant_3 = Plant("Cactus", 15, 120)
-print("=== Garden Plant Registry ===")
-plant_1.show()
-plant_2.show()
-plant_3.show()
+def protram_test() -> None:
+    plant_1 = Plant("Rose", 25, 30)
+    plant_2 = Plant("Sunflower", 80, 45)
+    plant_3 = Plant("Cactus", 15, 120)
+    print("=== Garden Plant Registry ===")
+    plant_1.show()
+    plant_2.show()
+    plant_3.show()
+
+
+if __name__ == "__main__":
+    protram_test()
