cd37129f8d4b25f8f59d37f56579a8ec353d2dcd
Oussama Amkhou <oamkhou@e1r3p14.1337.ma>
2026-01-21 21:56:49 +0100

    ex2 02 parsing part completed

--- module 03/ex2/ft_coordinate_system.py
+++ module 03/ex2/ft_coordinate_system.py
@@ -8,24 +8,37 @@ import math
 
 # print(distance)
 
+
 def parsing() -> tuple:
-    args = sys.argv.split()
+    args = sys.argv
     args_count = len(args) - 1
 
+    error = False
     try:
-        if args_count != 3:
-            raise Exception("coordinates must be in this form x, y, z")
-        coords = []
-        i = args_count
-        j = 1
-        while i > 0:
-            coords += [args[j]]
-            i -= 1
-            j += 1
+        if args_count < 1:
+            print("no coordinates given !")
+            return None
+        elif args_count > 1:
+            print("coordinates form must be as <x,y,z>")
+            return None
+        else:
+            coords = args[1].split(",")
+            result = []
+            for c in coords:
+                result += [int(c)]
+            result = tuple(result)
+            if len(result) != 3:
+                raise TypeError("coordinates must be 3 dimensions(x,y,z)")
     except Exception as e:
-        print(e)
+        error = True
+        print(f"Parsing invalid coordinates: \"{args[1]}\"")
+        print(f"Error parsing coordinates: {e}")
+        print(f"Error details - Type: {type(e).__name__}, Args: {e.args}")
     else:
-        print(coords)
+        if not error:
+            print(f"Parsing coordinates: \"{args[1]}\"")
+        print(result)
+
 
 if __name__ == "__main__":
     parsing()
